ae3828b4660aa1e300057cc547c0ad9c710320f0
kirubel <kiztopia.2020@gmail.com>
2022-01-20 19:16:18 +0300

    style: arrow img with style

--- sunnyside-react-app/src/home/components/Hero.js
+++ sunnyside-react-app/src/home/components/Hero.js
@@ -7,6 +7,9 @@ function Hero() {
       <img className='hero-desktop-img' src="images/desktop/image-header.jpg" alt="" />
         <Nav/>
       <h2 className='header'>We Are Creative </h2>
+      <div className="arrow-cont">
+      <img src="images/icon-arrow-down.svg" alt="|" className="arrow" />
+      </div>
   </div>;
 }
 
