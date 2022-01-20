57d34d7998dbf44b9f5eb0ec96577fb98dc6abd7
kirubel <kiztopia.2020@gmail.com>
2022-01-20 11:23:07 +0300

    config: custom react and README file added

---
+++ sunnyside-react-app/src/App.js
@@ -0,0 +1,22 @@
+
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+
+import './app.scss'
+// components
+
+
+// <Route path='/' element={<Home/>}/>
+
+function App() {
+  return (
+    <Router>
+
+      <Routes >
+
+      </Routes >
+    </Router>  
+    );
+}
+
+
+export default App;
