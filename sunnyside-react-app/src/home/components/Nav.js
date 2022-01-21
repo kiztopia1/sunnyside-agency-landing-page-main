6a0279436e28941df3e3546ef5034c32b07a36ad
kirubel <kiztopia.2020@gmail.com>
2022-01-21 09:35:07 +0300

    style: nav toggle and some style updated

--- sunnyside-react-app/src/home/components/Nav.js
+++ sunnyside-react-app/src/home/components/Nav.js
@@ -1,14 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './nav.scss'
 function Nav() {
+  const [toggle, setToggle] = useState('hide')
+  const handleToggle= ()=> {
+    if(toggle == 'hide'){
+      setToggle('')
+    }else{
+      setToggle('hide')
+    }
+  }
   return <div className='nav'>
       <img src="images/logo.svg" alt="sunnyside" />
-      <div className="menu">
-          <li>About </li>
-          <li>Services</li>
-          <li>Projects</li>
-          <li>Contact</li>
+      <div className={'menu'+ toggle}>
+          <li className={toggle}>About </li>
+          <li className={toggle}>Services</li>
+          <li className={toggle}>Projects</li>
+          <li className={toggle}>Contact</li>
       </div>
+      <img src="images/icon-hamburger.svg" alt="" className='hamburger' onClick={handleToggle} />
   </div>;
 }
 
